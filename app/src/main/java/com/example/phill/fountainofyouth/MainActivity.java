b6e70c0ae30d303fdb8ea21bc0432140a03ba750
lip ables <phillipables1@gmail.com>
2018-09-14 21:06:16 -0400

    onCreate find view by id for button and image view

--- app/src/main/java/com/example/phill/fountainofyouth/MainActivity.java
+++ app/src/main/java/com/example/phill/fountainofyouth/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.phill.fountainofyouth;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.animateBtn);
+        ImageView man = findViewById(R.id.imageView);
+        
+
     }
 }
